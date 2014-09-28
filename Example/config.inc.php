84c84f552b8e55283a6d6ede3b4c1aef87ad8f69
Bruno Ribeiro <offboard@hotmail.com>
2014-09-28 16:47:17 -0300

    Create config.inc.php

---
+++ Example/config.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Configuration for: Database
+ * This is the place where you define your database credentials, type etc.
+ *
+ * database host, usually it's "127.0.0.1" or "localhost", some servers also need port info, like "127.0.0.1:8080"
+ * define('DB_HOST', '127.0.0.1');
+ * name of the database. please note: database and database table are not the same thing!
+ * define('DB_NAME', 'login');
+ * user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT
+ * By the way, it's bad style to use "root", but for development it will work
+ * define('DB_USER', 'root');
+ * The password of the above user
+ * define('DB_PASS', 'xxx');
+ * define('DB_PREFIX', 'prefix_'); or NULL
+ */
+ 
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'table');
+define('DB_USER', 'root');
+define('DB_PASS', '');
