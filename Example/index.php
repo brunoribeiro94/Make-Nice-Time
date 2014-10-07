8968be41eaeaa32dbcb8b1bbeb5a7db32fb035fe
Bruno Ribeiro <offboard@hotmail.com>
2014-10-06 22:18:27 -0300

    Update index.php

--- Example/index.php
+++ Example/index.php
@@ -1,15 +1,13 @@
 <?php
-
+// include files required
 require '../libs/NiceDate.php';
 require 'config.inc.php';
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
 $query = mysqli_query($mysqli, "SELECT * FROM menu");
-$result = mysqli_fetch_array($query);
 
-
-foreach ($result as $value) {
+// list result(s)
+while($result = $mysqli_fetch_array($query)) {
     $dat = makeNiceTime::MakeNew($value['data']);
     echo $value['data'] . "<br>" . $dat . "<hr>";
 }
