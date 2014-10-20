199e40382ccc401d3040b49a109156163e2c2179
Bruno Ribeiro <offboard@hotmail.com>
2014-10-20 00:58:43 -0300

    Update index.php
    fix erro

--- Example/index.php
+++ Example/index.php
@@ -1,13 +1,14 @@
 <?php
 // include files required
 require '../libs/NiceDate.php';
+
 require 'config.inc.php';
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $query = mysqli_query($mysqli, "SELECT * FROM menu");
 
 // list result(s)
-while($result = $mysqli_fetch_array($query)) {
-    $dat = makeNiceTime::MakeNew($value['data']);
-    echo $value['data'] . "<br>" . $dat . "<hr>";
+while($result = mysqli_fetch_assoc($query)) {
+    $dat = makeNiceTime::MakeNew($result['data']);
+    echo "Timestamp : ".$result['data']." <br> Formatted : $dat <hr>";
 }
